0154a2b1d55ae66ae96ba08a6dd8463c43e9c5de
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2024-01-30 17:21:36 +0100

    Adding more UI work

--- WinVPN/Core/Extensions.cs
+++ WinVPN/Core/Extensions.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace WinVPN.Core
 {
     internal class Extensions
     {
-        // TODO: Implement Extensions
+        public static readonly DependencyProperty Icon = 
+            DependencyProperty.RegisterAttached("Icon", typeof(string), typeof(Extensions), new PropertyMetadata(default(string)));
+
+        public static void SetIcon(UIElement element, string value) => element.SetValue(Icon, value);
+
+        public static string GetIcon(UIElement element) => (string)element.GetValue(Icon);
     }
 }
