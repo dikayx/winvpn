5c32c33b07eb7a1d27393094f1626201e8bb94b8
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2024-01-30 14:55:49 +0100

    Initial commit

---
+++ WinVPN/Core/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WinVPN.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        // TODO: Maybe remove "this" keyword?
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null || this._canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this._execute(parameter);
+        }
+    }
+}
---
+++ WinVPN/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WinVPN.Core;
+
+namespace WinVPN.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        // Commands
+        public RelayCommand MoveWindowCommand { get; set; }
+        public RelayCommand ShutdownWindowCommand { get; set; }
+        public RelayCommand MaximizeWindowCommand { get; set; }
+        public RelayCommand MinimizeWindowCommand { get; set; }
+        public RelayCommand ShowProtectionView { get; set; }
+        public RelayCommand ShowSettingsView { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ProtectionViewModel ProtectionVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
+
+
+        public MainViewModel()
+        {
+            ProtectionVM = new ProtectionViewModel();
+            SettingsVM = new SettingsViewModel();
+            CurrentView = ProtectionVM;
+
+            Application.Current.MainWindow.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+
+            MoveWindowCommand = new RelayCommand(o => Application.Current.MainWindow.DragMove());
+            ShutdownWindowCommand = new RelayCommand(o => Application.Current.Shutdown());
+            // TODO: May need rework
+            //MaximizeWindowCommand = new RelayCommand(o => Application.Current.MainWindow.WindowState ^= WindowState.Maximized);
+            //MinimizeWindowCommand = new RelayCommand(o => Application.Current.MainWindow.WindowState = WindowState.Minimized);
+            MaximizeWindowCommand = new RelayCommand(o =>
+            {
+                if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+                {
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                }
+                else
+                {
+                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                }
+            });
+            MinimizeWindowCommand = new RelayCommand(o => Application.Current.MainWindow.WindowState = WindowState.Minimized);
+
+            ShowProtectionView = new RelayCommand(o => CurrentView = ProtectionVM);
+            ShowSettingsView = new RelayCommand(o => CurrentView = SettingsVM);
+        }
+    }
+}
