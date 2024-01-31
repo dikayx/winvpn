f1f79a49fe7086d2833df4e7844d2a3e2a34bdf0
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2024-01-31 14:11:33 +0100

    Finished basic design work

---
+++ WinVPN/MVVM/Model/ServerModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WinVPN.MVVM.Model
+{
+    public class ServerModel
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Server { get; set; }
+        public string Country { get; set; }
+    }
+}
--- WinVPN/MVVM/ViewModel/ProtectionViewModel.cs
+++ WinVPN/MVVM/ViewModel/ProtectionViewModel.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WinVPN.MVVM.Model;
 
 namespace WinVPN.MVVM.ViewModel
 {
     internal class ProtectionViewModel
     {
+        public ObservableCollection<ServerModel> Servers { get; set; }
         public ProtectionViewModel()
         {
-
+            Servers = new ObservableCollection<ServerModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                Servers.Add(new ServerModel
+                {
+                    Country = "USA"
+                });
+            }
         }
     }
 }
